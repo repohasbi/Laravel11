de1f7a87cfff93c062542264625a7c6b7169577a
repohasbi <codepolitan.hasby@gmail.com>
2024-05-15 14:10:11 +0700

    Blade Component

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }}</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-    <h1>Halaman {{ $title }}</h1>
-    <article>
-        <h3>Articles</h3>
-        @foreach ($articles as $article)
-            <h4>{{ $article }}</h4>
-            @foreach ($posts as $post)
-                <p>{{ $post }}</p>
-            @endforeach
-        @endforeach
-    </article>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3 class="text-xl">Ini Blog</h3>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['title' => 'Home']);
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
     return view('about', [
         'title' => 'About',
-        'nama' => 'Hasbi',
+        'name' => 'Hasbi',
     ]);
 });
 
